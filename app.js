61d6a06ef63ec55383176cfb0120a2c06937b2f8
DashKiwi <Maxmooney08@gmail.com>
2024-05-20 13:28:28 +1200

    Made it look better
    Made the website home screen look better and work better with mobile support
    Made the text on the ocean menu go below the other text

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 
+// When the user clicks the lean more button it scrolls to
+// to the learn more options
+function infoFunction() {
+    console.log("Working")
+    document.body.scrollTop = 800 // For Safari
+    document.documentElement.scrollTop = 800; // For everything else
+}
+
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
